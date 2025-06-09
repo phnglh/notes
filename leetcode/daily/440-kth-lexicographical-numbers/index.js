9a3c1b904b81ccb7278ee82b4c13e61e8671111e
phonglh <phonghngle@gmail.com>
2025-06-10 00:49:10 +0700

    Add optimized findKthNumber solution without memory overflow

--- leetcode/daily/440-kth-lexicographical-numbers/index.js
+++ leetcode/daily/440-kth-lexicographical-numbers/index.js
@@ -4,42 +4,38 @@
  * @return {number}
  */
 
-// Brute force
 var findKthNumber = function (n, k) {
-  const arr = [];
-  for (let i = 1; i <= n; i++) {
-    arr.push(i);
-  }
-  arr.sort();
-  return arr[k - 1];
-};
+  let curr = 1;
+  k--; 
 
-// DFS cơ bản
-var findKthNumber = function (n, k) {
-  const result = [];
-
-  function dfs(current) {
-    if (current > n) return;
-    result.push(current);
-
-    for (let i = 0; i <= 9; i++) {
-      const next = current * 10 + i;
-      if (next > n) break;
-      dfs(next);
+  while (k > 0) {
+    const steps = calcSteps(n, curr, curr + 1);
+    if (steps <= k) {
+      curr++;
+      k -= steps;
+    } else {
+      curr *= 10;
+      k--;
     }
   }
 
-  for (let i = 1; i <= 9; i++) {
-    if (i > n) break;
-    dfs(i);
-  }
+  return curr;
 
-  return result[k - 1];
+  function calcSteps(n, curr, next) {
+    let steps = 0;
+    while (curr <= n) {
+      steps += Math.min(n + 1, next) - curr;
+      curr *= 10;
+      next *= 10;
+    }
+    return steps;
+  }
 };
 
 function test() {
   console.log("Test n=13, k=2:", findKthNumber(13, 2)); 
-  console.log("Test n=1, k=1:", findKthNumber(1, 1)); 
+  console.log("Test n=1, k=1:", findKthNumber(1, 1));   
+  console.log("Test n=1000000000, k=100000000"); 
 }
 
 test();
