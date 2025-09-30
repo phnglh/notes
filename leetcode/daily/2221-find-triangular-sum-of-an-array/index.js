2704c240c1b22b13f7d58bc8a1515cbbe12a81fc
phonglh <phonghngle@gmail.com>
2025-09-30 21:52:26 +0700

    2221

---
+++ leetcode/daily/2221-find-triangular-sum-of-an-array/index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var triangularSum = function(nums) {
+    while (nums.length > 1) {
+        let next = [];
+        for (let i = 0; i < nums.length - 1; i++) {
+            next.push((nums[i] + nums[i + 1]) % 10);
+        }
+        nums = next;
+    }
+    return nums[0];
+};
+
